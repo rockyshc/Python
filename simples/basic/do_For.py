d20b7014ca1763e97a9148f80293c74922fb44f6
Rocky Shen <rocky.shen@sap.com>
2017-11-29 16:05:10 +0800

    Add for

---
+++ simples/basic/do_For.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# for x in y
+names = ['Tom', 'Mike', 'Mary']
+for name in names:
+    print(name)
+
+# range(n) for 0 to n and put into list
+print(list(range(5)))
+
+#
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
