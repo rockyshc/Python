9eca5272dbe173e4c77302e2828b7907a0697665
Rocky Shen <rocky.shen@sap.com>
2017-11-29 17:57:18 +0800

    Add call function

---
+++ function/call_Func.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# https://docs.python.org/3/library/functions.html
+# Can use help(functionName) in Python command line
+
+# absolute value
+print(abs(-20))
